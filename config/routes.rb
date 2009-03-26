d5d13edd37a72c12755fa9f808d078616253b2a5
Marat Stary <maratstary@marat-starys-macbook-pro.local>
2009-03-26 17:41:29 -0400

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ ActionController::Routing::Routes.draw do |map|
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
   map.connect ':controller/service.wsdl', :action => 'wsdl'
+  
+  #my code - marat
+  map.root :controller => "home"
 
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
